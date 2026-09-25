d1a3f27d0189dd56a48429b65b86d08069a5585e
ArianSdg <ariansadegh8305@gmail.com>
2026-09-25 23:12:49 +0330

    Rename health function

--- tests/test_api.py
+++ tests/test_api.py
@@ -3,7 +3,7 @@ from fastapi.testclient import TestClient
 from app.main import app
 
 
-def test_get_health():
+def test_health():
     client = TestClient(app)
 
     response = client.request(method="get", url="/health")
